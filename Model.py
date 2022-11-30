03f1c80fda76d96016711adbccf9695c666c848b
Ahmad <1ahmadkamal1@gmail.com>
2022-11-30 13:30:53 -0500

    finished writing the class. untested

--- Model.py
+++ Model.py
@@ -10,6 +10,7 @@ from torch.utils.data import dataset
 
 class Model(nn.Module):
     def __init__(self,
+                 seq_length: int,
                  d_model: int,
                  nhead: int,
                  d_feed_forward: int,
@@ -37,13 +38,17 @@ class Model(nn.Module):
 
         self.all_conv = ConvBlock(d_conv_layers*4, d_model, num_all_conv_layers, kernel_size)
 
-        self.pos_encoder = PositionalEncoding(d_model, encoder_dropout)
+        self.pos_encoder = PositionalEncoding(d_model, encoder_dropout, seq_length=seq_length)
         encoder_layers = TransformerEncoderLayer(d_model, nhead, d_feed_forward, encoder_dropout, batch_first=True)
         self.transformer_encoder = TransformerEncoder(encoder_layers, n_encoders)
         self.d_model = d_model
 
         self.max_pool = nn.MaxPool1d(max_pool_dim, stride=max_pool_dim)
-        # TODO: max pool, MLP head, softmax
+
+        mlp_input_dimensions = d_model * seq_length//max_pool_dim  # the output of max pool will be flattened to this
+        self.mlp = MLP(mlp_input_dimensions, 1, d_mlp, n_mlp_layers)
+
+        self.sigmoid = nn.Sigmoid()
 
         self.init_weights()
 
@@ -57,22 +62,20 @@ class Model(nn.Module):
                 emg: Tensor,
                 ecg: Tensor,
                 imu: Tensor,
-                skin: Tensor,
-                mask: Tensor) -> Tensor:
+                skin: Tensor,) -> Tensor:
         """
 
         :param emg: Tensor, shape [batch_size, d_emg, seq_length]
         :param ecg: Tensor, shape [batch_size, d_ecg, seq_length]
         :param imu: Tensor, shape [batch_size, d_imu, seq_length]
         :param skin: Tensor, shape [batch_size, d_skin, seq_length]
-        :param mask: Tensor, shape [batch_size, d_mask, seq_length]
         :return: Tensor, shape [batch_size, 1]
         """
         # run each input through a convolution block
         emg_out = self.emg_conv(emg)  # output shape [batch_size, d_conv_layers, seq_length]
-        ecg_out = self.ecg_conv(emg)  # output shape [batch_size, d_conv_layers, seq_length]
-        imu_out = self.imu_conv(emg)  # output shape [batch_size, d_conv_layers, seq_length]
-        skin_out = self.skin_conv(emg)  # output shape [batch_size, d_conv_layers, seq_length]
+        ecg_out = self.ecg_conv(ecg)  # output shape [batch_size, d_conv_layers, seq_length]
+        imu_out = self.imu_conv(imu)  # output shape [batch_size, d_conv_layers, seq_length]
+        skin_out = self.skin_conv(skin)  # output shape [batch_size, d_conv_layers, seq_length]
 
         # concatanate inputs. output shape [batch_size, 4*d_conv_layers, seq_length]
         data = torch.cat([emg_out, ecg_out, imu_out, skin_out], dim=1)
@@ -87,21 +90,21 @@ class Model(nn.Module):
         data = self.pos_encoder(data)  # output shape [batch_size, seq_length, d_model]
 
         # run through the transformer layers
-        # TODO: check dimensions of mask. do we need it?
-        data = self.transformer_encoder(data, mask)  # output shape [batch_size, seq_length, d_model]
+        data = self.transformer_encoder(data)  # output shape [batch_size, seq_length, d_model]
+
+        # swap the seq_length and d_model axes because of the expected shape for max pool
+        data = torch.swapaxes(data, 1, 2)  # output shape [batch_size, d_model, seq_length]
 
         # run through a max pool to reduce the dimensions
-        data = self.max_pool(data)
+        data = self.max_pool(data)  # output shape [batch_size, d_model, seq_length//max_pool_dim]
 
         # rearrange the data to 1D to prep for the MLP
-        data = torch.flatten(data, 1, 2)  # output shape [batch_size, seq_length * d_model]
+        data = torch.flatten(data, 1, 2)  # output shape [batch_size, d_model * seq_length//max_pool_dim]
 
         # run through the MLP
-        data = self.mlp(data)
+        data = self.mlp(data)  # output shape [batch_size, 1]
 
-        # todo: run through mlp
-
-        return data
+        return self.sigmoid(data)
 
 
     # def forward(self, src: Tensor, src_mask: Tensor) -> Tensor:
@@ -126,11 +129,27 @@ class ConvBlock(nn.Module):
         other_layers = [nn.Conv1d(d_out, d_out, kernel_size) for _ in range(1, n_layers)]
         self.layers = [first_layer] + other_layers
 
-    def forward(self, input: Tensor) -> Tensor:
-        x = input
+    def forward(self, x: Tensor) -> Tensor:
         for layer in self.layers:
             x = layer(x)
+            x = nn.ReLU()(x)
         return x
+
+
+class MLP(nn.Module):
+    def __init__(self, d_input: int, d_out: int, d_hidden: int, n_layers: int):
+        self.layers = []
+        self.layers.append(nn.Linear(d_input, d_hidden))
+        for _ in range(2, n_layers):  # num hidden layers = num layers - 2
+            self.layers.append(nn.Linear(d_hidden, d_hidden))
+        self.layers.append(nn.Linear(d_hidden, d_out))
+
+    def forward(self, x: Tensor) -> Tensor:
+        for layer in self.layers[:-1]:
+            x = layer(x)
+            x = nn.ReLU()(x)
+        output = self.layers[-1](x)
+        return output
 
 
 def generate_square_subsequent_mask(sz: int) -> Tensor:
@@ -139,22 +158,16 @@ def generate_square_subsequent_mask(sz: int) -> Tensor:
 
 
 class PositionalEncoding(nn.Module):
-    # TODO: rearrange axis to be [batch_size, seq_length, d_model]
-    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+    def __init__(self, d_model: int, dropout: float = 0.1, seq_length: int = 5000):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
-
-        position = torch.arange(max_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
-        pe = torch.zeros(max_len, 1, d_model)
-        pe[:, 0, 0::2] = torch.sin(position * div_term)
-        pe[:, 0, 1::2] = torch.cos(position * div_term)
-        self.register_buffer('pe', pe)
+        self.pos = nn.Parameter(torch.randn(d_model, seq_length))
+        self.pos.requires_grad = True
 
     def forward(self, x: Tensor) -> Tensor:
         """
         Args:
-            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+            x: Tensor, shape [batch_size, seq_length, d_model]
         """
-        x = x + self.pe[:x.size(0)]
+        x = x + self.pos
         return self.dropout(x)
