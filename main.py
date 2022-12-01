6a1b81430f49b224fd97128fa579bf48bed57a8d
Emily Wang <write2emily@hotmail.com>
2022-12-01 13:39:48 -0500

    WIP optuna

--- main.py
+++ main.py
@@ -13,50 +13,56 @@ def main():
 def objective(trial):
     #optuna: window size, sequence length, d_model, nhead,n_encoders
     #constant parameters: d_feed_forward: kernel size, d_conv_layers, step size = 1/4 window
-        cfg = {     'seq_length': trial.suggest_categorical(256, 512, 2048),
-                    'd_model' : trial.suggest_categorical(16, 32),
-                    'nhead' : trial.suggest_categorical(2,4,8),
-                    'n_encoders' : trial.suggest_categorical(1,3)
-                }
+    cfg = {     'seq_length': trial.suggest_categorical('seq_length', 256, 512, 2048),
+                    'd_model' : trial.suggest_categorical('d_model', 16, 32),
+                    'nhead' : trial.suggest_categorical('nhead', 2,4,8),
+                    'n_encoders' : trial.suggest_int('n_encoders', 1,3)
+            }
+    NUM_EPOCH = 5
 
-        train_loader, test_loader = get_loaders(cfg['train_batch_size'], cfg['test_batch_size'], cfg['aug_deg'], cfg['aug_trans'], cfg['aug_scale'])
-        model = Network(cfg['activation'], trial, cfg['dropout'])
+    d_feed_forward = 512
+    d_emg = 10
+    d_ecg = 10
+    d_imu = 10
+    d_skin = 10
+    num_init_conv_layers = 3
+    num_all_conv_layers = 2
+    kernel_size = 8
+    d_conv_layers = 32
+    encoder_dropout = 0.5
+    max_pool_dim = 4
+    d_mlp = 128
+    n_mlp_layers = 2
+    model = Model(seq_length=cfg['seq_length'], d_model=cfg['d_model'], nhead=cfg['nhead'], d_feed_forward, d_emg, d_ecg, max_pool_dim, num_init_conv_layers, kernel_size, num_all_conv_layers, d_conv_layers, encoder_dropout, d_mlp, n_mlp_layers)
+    max_test_accuracy = 0
 
-        study = optuna.create_study(sampler=sampler, direction='maximize')
-        study.optimize(func=objective, n_trials=20)
+    epoch = 0
+    test_loss = None
+    best_test_loss = 69696969696969
+    num_non_decreasing_loss = 0
+    patience = 3
 
+    while num_non_decreasing_loss < patience and epoch < NUM_EPOCH:
+        train(model, train_loader, epoch)
+        test_loss = validate(model, validation_loader)
+
+        if test_loss < best_test_loss:
+            best_test_loss = test_loss
+            num_non_decreasing_loss = 0
+        else:
+            num_non_decreasing_loss += 1
+        epoch += 1
+
+    return max_test_accuracy
+
+def optimize_with_optuna():
+
+    sampler = optuna.samplers.TPESampler()
+    study = optuna.create_study(sampler=sampler, direction='maximize')
+    study.optimize(func=objective, n_trials=20)
 
 
 if __name__ == '__main__':
+
     main()
 
-# def train_mnist(trial):
-
-#   cfg = { 'device' : "cuda" if torch.cuda.is_available() else "cpu",
-#           'train_batch_size' : trial.suggest_categorical('train_batch_size', [64, 128, 256]),
-#           'test_batch_size' : 1000,
-#           'aug_deg' : 20,
-#           'aug_trans' : trial.suggest_uniform('aug_trans', 0, 0.3),
-#           'aug_scale' : trial.suggest_uniform('aug_scale', 0, 0.3),
-#           'l2_weight_decay' : trial.suggest_loguniform('l2_weight_decay', 1e-6, 1e-1),
-#           'dropout' : trial.suggest_uniform('dropout', 0, 0.5),
-#           'max_epochs' : 5,
-#           'activation': F.relu}
-
-#   train_loader, test_loader = get_loaders(cfg['train_batch_size'], cfg['test_batch_size'], cfg['aug_deg'], cfg['aug_trans'], cfg['aug_scale'])
-#   model = Network(cfg['activation'], trial, cfg['dropout'])
-#   model.to('cuda:0')
-#   optimizer = optim.Adam(model.parameters(), weight_decay=cfg['l2_weight_decay'])
-
-#   max_test_accuracy = 0
-#   for epoch in range(1, cfg['max_epochs'] + 1):
-#       stop = False
-#       train(model, train_loader, optimizer, epoch)
-#       test_accuracy = test(model, test_loader)
-
-#       max_test_accuracy = max(max_test_accuracy,test_accuracy)
-#       trial.report(max_test_accuracy, epoch)
-#       if trial.should_prune():
-#             raise optuna.exceptions.TrialPruned()
-
-#   return max_test_accuracy
